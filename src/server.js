572a92d7aa3fbdd60814b6a562e0d2f7020a1fe8
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2024-06-05 22:48:44 +0300

    Initial commit

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ import pino from "pino";
 import pinoHttp from "pino-http";
 import cors from "cors";
 // import dotevn from "dotenv";
-import {evn} from "./utils/evn.js";
-import { ENV_VARS } from "./constans/index.js";
+// import {evn} from "./utils/evn.js";
+//
+
 import {initMongoDb} from "./db/initMongoConnection.js";
 import {getContacts, getContactById} from "./services/contacts.js";
 import mongoose from "mongoose";
@@ -33,9 +34,7 @@ return res.status(200).json(contact);
 });
 app.use('*', (req, res)=>{
     res.status(404).json({message: "Not found"});});
-    const PORT =evn(ENV_VARS.PORT, 3000);
-    // process.env.PORT || 3000;
-    console.log('PORT', PORT);
+    const PORT = process.env.PORT || 3000;
     app.use((err,req, res)=>{
 res.status(500).join({message:'Someting went wrong'});
     });
