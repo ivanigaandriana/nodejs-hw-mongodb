b9f0f7e1d2c702f4aecf487fa4ee60bed250ec56
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2024-06-09 15:39:38 +0300

    change server.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { startServer } from './src/server.js';
 import { initMongoDb } from './src/db/initMongoConnection.js';
+import './server.js';
 
 const bootstrap = async() => {
     await initMongoDb();
