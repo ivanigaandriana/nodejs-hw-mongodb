c7becacf2e87a3b1eef531cd50efe31437cc37ad
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2024-06-09 15:50:59 +0300

    f

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { startServer } from './src/server.js';
+import { startServer } from './server.js';
 import { initMongoDb } from './src/db/initMongoConnection.js';
-import './server.js';
+
 
 const bootstrap = async() => {
     await initMongoDb();
