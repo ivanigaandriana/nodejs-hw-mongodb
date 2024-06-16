f3664feb565b09ccf54ffa6b271572609565662c
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2024-06-16 14:55:10 +0300

    delet 1 contact

--- src/middlewares/errorHandlerMiddlever.js
+++ src/middlewares/errorHandlerMiddlever.js
@@ -8,5 +8,4 @@ const errorHandlerMiddleware = (err, req, res, next) => {
 
     res.status(500).json({ status: 500, message: 'Something went wrong', data: err });
 };
-
 export default errorHandlerMiddleware;
