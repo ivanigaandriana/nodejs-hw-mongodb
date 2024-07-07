59f39f2fc3a2d339dc85fd019d0ef8e730941322
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2024-07-07 10:35:16 +0300

    vbhj

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,6 +4,10 @@ import{Contacts }from "../db/Models/Contact.js";
 import { calculatePaginationData } from "../utils/calculatePaginationData.js";
 import { saveFile } from "../utils/saveFile.js";
 
+import { env } from "../utils/evn.js";
+import saveFileToLocalStorage from "../utils/saveFileToLocalStorage.js";
+import saveFileToCloudinary from "../utils/saveFileToCloudinary.js";
+import { ENV_VARS } from "../constans/index.js";
 
 export const getAllContacts = async ({
     page = 1,
@@ -34,30 +38,43 @@ export const getAllContacts = async ({
     // const { photo, ...restPayload } = payload;
 
     // const photoUrl = photo ? await saveFile(photo) : null;
-const url =await saveFile(photo);
 
+    if (!photo) return;
+
+    let photoUrl;
+
+    if (photo) {
+        if (env(ENV_VARS.ENABLE_CLOUDINARY) === 'true') {
+            photoUrl = await saveFileToCloudinary(photo);
+        } else {
+            photoUrl = await saveFileToLocalStorage(photo);
+        }
+    }
     const contact = await Contacts.create({
-        ...payload,
-        userId,
-        photo: url
-    });
+      ...payload,
+      userId: userId,
+      photo: photoUrl,
+  });
 
-    return contact;
+  return contact;
 };
+// const url =await saveFile(photo);
+
+//     const contact = await Contacts.create({
+//         ...payload,
+//         userId,
+//         photo: url
+//     });
+
+//     return contact;
+// };
 
   export const deleteContact = async (contactId, userId) => {
     const contact = await Contacts.findOneAndDelete({ _id: contactId,  userId });
     return contact;
   };
 
-  // export const updateContact = async (contactId, updateData, userId) => {
-  //   const contact = await ContactsCollection.findOneAndUpdate(
-  //     { _id: contactId,  userId },
-  //     { $set: updateData },
-  //     { new: true, runValidators: true }
-  //   );
-  //   return contact;
-  // };
+
   export const updateContact = async (contactId, payload) => {
     const { photo, ...restPayload } = payload;
     const updateFields = { ...restPayload };
