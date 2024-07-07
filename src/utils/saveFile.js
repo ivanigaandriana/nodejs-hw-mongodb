3a3e376449b23caf2a1cbc71d85320db2bccfb71
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2024-07-07 10:18:52 +0300

    change dg

--- src/utils/saveFile.js
+++ src/utils/saveFile.js
@@ -1,16 +1,17 @@
 import { ENV_VARS } from "../constans/index.js";
 import { saveFileToCloudinary } from "../utils/saveFileToCloudinary.js";
 import { saveFileToLocalStorage } from "../utils/saveFileToLocalStorage.js";
-import { env } from "../utils/env.js";
+import { env } from "../utils/evn.js";
 
-
-export const saveFile = async (file) => {
+ export const saveFile =async (file) => {
     let url;
     const isCloudinary = env(ENV_VARS.ENABLED_CLOUDINARY);
-    if (isCloudinary === 'true') {
+    if(isCloudinary === 'true') {
         url = await saveFileToCloudinary(file);
     } else {
         url = await saveFileToLocalStorage(file);
     }
     return url;
-};
+ };
+
+
