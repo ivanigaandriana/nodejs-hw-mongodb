fddb4e1507a4daa008a9106449aadd5d4e6f01c0
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2024-07-10 15:37:16 +0300

    change cloudinary

--- src/utils/saveFile.js
+++ src/utils/saveFile.js
@@ -3,14 +3,17 @@ import { saveFileToCloudinary } from "../utils/saveFileToCloudinary.js";
 import { saveFileToLocalStorage } from "../utils/saveFileToLocalStorage.js";
 import { env } from "../utils/evn.js";
 
- export const saveFile =async (file) => {
+export const saveFile = async (file) => {
     let url;
     const isCloudinary = env(ENV_VARS.ENABLED_CLOUDINARY);
-    if(isCloudinary === 'true') {
+    console.log(`ENABLED_CLOUDINARY: ${isCloudinary}`);
+    if (isCloudinary === 'true') {
+        console.log('Uploading to Cloudinary');
         url = await saveFileToCloudinary(file);
     } else {
+        console.log('Saving to local storage');
         url = await saveFileToLocalStorage(file);
     }
     return url;
- };
+};
 
