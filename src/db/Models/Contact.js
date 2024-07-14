bf31b2bd838f9a1e90944bc6ec3208d9827d2e3a
ivanigaandriana <110620062+ivanigaandriana@users.noreply.github.com>
2024-07-14 10:41:51 +0300

    change post

--- src/db/Models/Contact.js
+++ src/db/Models/Contact.js
@@ -16,10 +16,10 @@ import {Schema, model} from "mongoose";
  export const contactModel = new Schema({
     name:{type: String, required: true},
     phoneNumber:{type: String, required: true},
-    email:{type: String, required: false,},
+    email:{type: String},
     isFavourite:{type: Boolean, default: false,},
-    contactType:{type: String, required: true, default: "personal", enum: ["personal", "home", "work"]},
-    userId: { type: Schema.Types.ObjectId, required: true },
+    contactType:{type: String,  default: "personal", enum: ["personal", "home", "work"]},
+    userId: { type: Schema.Types.ObjectId,  },
     photo: {type: String,},},
     {
         timestamps: true, versionKey: false,});
